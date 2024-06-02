da38d857b0101271521d98e05ee4fe45f6667886
Luisa Cavalheiro <luisacavalheiro@gmail.com>
2024-06-02 19:54:13 -0300

    Fix db

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import 'dotenv/config.js'
 import express from 'express'
 import cors from 'cors'
 
-import PacienteController from './controller/PacienteController.js'; 
+import PacientesController from './controller/PacientesController.js'; 
+import MedicosController from './controller/MedicosController.js';
 
 let servidor = express();
 servidor.use( cors());
 servidor.use(express.json()); 
 
-servidor.use(PacienteController);
+servidor.use(PacientesController);
+servidor.use(MedicosController);
 
 let porta = process.env.PORTA;
 servidor.listen(porta, ()=> console.log(`API subiu na porta ${porta}`))
